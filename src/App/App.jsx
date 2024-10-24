f9bb70e8a73e9a1b520b3354b8570ea8f7c3c438
TomaszSkiers <tomasz.skiers@gmail.com>
2024-10-24 20:45:21 +0200

    dist test

---
+++ src/App/App.jsx
@@ -0,0 +1,32 @@
+import s from "./app.module.scss"
+
+export default function App() {
+  return (
+    <>
+      <div className={s.container}>
+        <img src="/photos/bones.png"></img>
+        <img src="/photos/brain.png"></img>
+        <img src="/photos/heart.png"></img>
+        <img src="/photos/kidney.png"></img>
+      </div>
+      <div className={s.container}>
+        <img src="/photos/bones.png"></img>
+        <img src="/photos/brain.png"></img>
+        <img src="/photos/heart.png"></img>
+        <img src="/photos/kidney.png"></img>
+      </div>
+      <div className={s.container}>
+        <img src="/photos/bones.png"></img>
+        <img src="/photos/brain.png"></img>
+        <img src="/photos/heart.png"></img>
+        <img src="/photos/kidney.png"></img>
+      </div>
+      <div className={s.container}>
+        <img src="/photos/bones.png"></img>
+        <img src="/photos/brain.png"></img>
+        <img src="/photos/heart.png"></img>
+        <img src="/photos/kidney.png"></img>
+      </div>
+    </>
+  )
+}

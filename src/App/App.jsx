d978dfc6f4850c92db2f19a8ef02a1b6845d9321
TomaszSkiers <tomasz.skiers@gmail.com>
2024-10-24 20:58:20 +0200

    dist test

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,28 +4,28 @@ export default function App() {
   return (
     <>
       <div className={s.container}>
-        <img src="/photos/bones.png"></img>
-        <img src="/photos/brain.png"></img>
-        <img src="/photos/heart.png"></img>
-        <img src="/photos/kidney.png"></img>
+        <img src="./photos/bones.png"></img>
+        <img src="./photos/brain.png"></img>
+        <img src="./photos/heart.png"></img>
+        <img src="./photos/kidney.png"></img>
       </div>
       <div className={s.container}>
-        <img src="/photos/bones.png"></img>
-        <img src="/photos/brain.png"></img>
-        <img src="/photos/heart.png"></img>
-        <img src="/photos/kidney.png"></img>
+        <img src="./photos/bones.png"></img>
+        <img src="./photos/brain.png"></img>
+        <img src="./photos/heart.png"></img>
+        <img src="./photos/kidney.png"></img>
       </div>
       <div className={s.container}>
-        <img src="/photos/bones.png"></img>
-        <img src="/photos/brain.png"></img>
-        <img src="/photos/heart.png"></img>
-        <img src="/photos/kidney.png"></img>
+        <img src="./photos/bones.png"></img>
+        <img src="./photos/brain.png"></img>
+        <img src="./photos/heart.png"></img>
+        <img src="./photos/kidney.png"></img>
       </div>
       <div className={s.container}>
-        <img src="/photos/bones.png"></img>
-        <img src="/photos/brain.png"></img>
-        <img src="/photos/heart.png"></img>
-        <img src="/photos/kidney.png"></img>
+        <img src="./photos/bones.png"></img>
+        <img src="./photos/brain.png"></img>
+        <img src="./photos/heart.png"></img>
+        <img src="./photos/kidney.png"></img>
       </div>
     </>
   )
